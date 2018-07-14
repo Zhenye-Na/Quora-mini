92b7c79c9f53d4610bf3c2e84a3979667800decd
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-07-14 16:53:54 -0500

    Update routes

--- quora-mini/app/Http/routes.php
+++ quora-mini/app/Http/routes.php
@@ -11,30 +11,85 @@
 |
 */
 
+/** Create user instance */
+
 function user_init() {
-    return $user = new App\User;
+    return new App\User;
 }
 
+
+/** Create Question instance */
+
+function question_init() {
+    return new App\Question;
+}
+
+
+/** Request all */
+function rq($key=null, $default=null) {
+    if (!$key) return Request::all();
+    return Request::get($key, $default);
+}
+
+
+
+/** homepage */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+/** Sign up page */
+
 Route::any('api/signup', function() {
     return user_init()->signup();
 });
 
+
+/** Log in page */
 
 Route::any('api/login', function() {
     return user_init()->login();
 });
 
 
+/** Log put page */
+
 Route::any('api/logout', function() {
     return user_init()->logout();
 });
 
+
+/** Create question */
+
+Route::any('api/question/create', function() {
+    return question_init()->add();
+});
+
+
+/** Update question */
+
+Route::any('api/question/change', function() {
+    return question_init()->change();
+});
+
+
+/** Read question */
+
+Route::any('api/question/read', function() {
+    return question_init()->read();
+});
+
+
+/** Delete question */
+
+Route::any('api/question/remove', function() {
+    return question_init()->remove();
+});
+
+
+/** Hold for testing functionality */
 
 Route::any('test', function() {
     dd(user_init()->is_logged_in());
