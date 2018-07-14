27953d43047c78f703f5d6e29ce209e8c6d428e5
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-07-13 21:26:23 -0500

    Add Login API

--- quora-mini/app/User.php
+++ quora-mini/app/User.php
@@ -5,42 +5,58 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Request;
 use Hash;
+use DB;
 
 class User extends Model
 {
     
-    /* Register API */
+    /* Examine username and password is valid */
     
-    public function signup() {
+    public function isValid() {
 
         $username = Request::get('username');
         $password = Request::get('password');
 
+        if ($username && $password)
+            return [$username, $password];
+
+        return false;
+    }
+
+
+    /* Register API */
+    
+    public function signup() {
+
         /* 1. Examine whether username is null */
+        /* 2. Examine whether password is null */
 
-        if (!$username)
-            return ['status' => 0, 'msg' => 'Username cannot be empty!'];
+        $examination = $this->isValid();
 
-        /* 1. Examine whether password is null */
+        if (!$examination)
+            return ['status' => 0, 'msg' => 'Username or passsword is empty!'];
 
-        if (!$password)
-            return ['status' => 0, 'msg' => 'Password cannot be empty!'];
+        $username = $examination[0];
+        $password = $examination[1];
 
-        /* 2. Examine whether username is valid */
 
-        $user_exists = $this
+        /* 3. Examine whether username is valid */
+
+        $user_exists = DB::table('users')
             ->where('username', $username)
             ->exists();
 
         if ($user_exists)
             return ['status' => 0, 'msg' => 'Username already exists'];
 
-        /* 3. Encrypt password and save into database */
+
+        /* 4. Encrypt password and save into database */
 
         $hashed_password = Hash::make($password);  // Also bcrypt($password)
-        $this->password = $hashed_password;
-        $this->username = $username;
-        if ($this->save())
+        $user = $this;
+        $user->password = $hashed_password;
+        $user->username = $username;
+        if ($user->save())
             return ['status' => 1, 'id' => $user->id];
         else
             return ['status' => 0, 'msg' => 'Database insert failed!'];
@@ -48,5 +64,48 @@ class User extends Model
     }
 
 
+
+    /* Login API */
+
+    public function login() {
+
+        /* 1. Examine whether username and password */
+
+        $examination = $this->isValid();
+
+        if (!$examination)
+            return ['status' => 0, 'msg' => 'Username or passsword is empty!'];
+
+        $username = $examination[0];
+        $password = $examination[1];
+
+        $user = DB::table('users')
+            ->where('username', $username)
+            ->first();
+
+
+        /* 2. Examine whether user exists */
+
+        if (!$user)
+            return ['status' => 0, 'msg' => 'User not exists!'];
+
+
+        /* 3. Examine whether password is correct */
+
+        $hashed_password = $user->password;
+
+        if (!Hash::check($password, $hashed_password))
+            return ['status' => 0, 'msg' => 'password or username is not correct!'];
+
+        /* 4. Save user info to session */
+
+        session()->put('username', $user->username);
+        session()->put('user_id', $user->id);
+
+        dd(session()->all());
+
+
+        return ['status' => 1, 'id' => $user->id];
+    }
     
 }
